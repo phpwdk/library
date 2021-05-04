394f182e652d7f0c4ae8e21dcaabaa071d2c91a8
phpwdk <9653992@qq.com>
2021-05-04 15:14:40 +0800

    添加文件

---
+++ Util/Console.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * 版权所有 ( 汪登科 )
+ * 代码维护 ( 汪登科 )
+ * 开源协议 ( https://mit-license.org )
+ * 联系微信 ( wk9653992 )
+ * 启动日期 ( 2021/5/4 14:06 )
+ */
+
+namespace Util;
+
+class Console
+{
+    public static function logs(...$data)
+    {
+        $log = func_get_args();
+        $logStr = "";
+        foreach ($log as $value) {
+            $logStr .= self::getStr($value) . " ";
+        }
+        echo $logStr . "\n";
+    }
+
+    public static function log($data)
+    {
+        $data = self::getStr($data);
+        echo $data . "\n";
+    }
+
+    public static function logStr($data, $ln = false)
+    {
+        if (!is_string($data)) {
+            return;
+        }
+        echo $data;
+        if ($ln) {
+            echo "\n";
+        }
+    }
+
+    private static function getStr($data)
+    {
+        /*
+         * "boolean"
+         * "integer"
+         * "double" (for historical reasons "double" is
+         * returned in case of a float, and not simply
+         * "float")
+         * "string"
+         * "array"
+         * "object"
+         * "resource"
+         * "NULL"
+         * "unknown type"
+         * "resource (closed)" since 7.2.0*/
+        if (gettype($data) == "array") {
+            $data = json_encode($data);
+        } elseif (gettype($data) == "object") {
+            $data = json_encode($data);
+        }
+        return $data;
+    }
+}
---
+++ Util/Logger.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * 版权所有 ( 汪登科 )
+ * 代码维护 ( 汪登科 )
+ * 开源协议 ( https://mit-license.org )
+ * 联系微信 ( wk9653992 )
+ * 启动日期 ( 2021/5/4 14:00 )
+ */
+
+namespace Util;
+
+class Logger
+{
+    const LOGGER_PREFIX = '/var/log/cloud/';
+
+    public static $_LOGGER_DIR = "";
+
+    private static $_logs = [];
+    private static $logLevel = [
+        LOG_EMERG => 'EMERG',
+        LOG_CRIT => 'CRIT',
+        LOG_ERR => 'ERROR',
+        LOG_WARNING => 'WARNING',
+        LOG_NOTICE => 'NOTICE',
+        LOG_INFO => 'INFO',
+        LOG_DEBUG => 'DEBUG',
+    ];
+
+    public static function GetLogs()
+    {
+        $log = self::$_logs;
+        $log[] = ["hostname:" . gethostname()];
+        self::$_logs = [];
+        return $log;
+    }
+
+    public static function clearLogs()
+    {
+        self::$_logs = [];
+    }
+
+    public static function EMERG($logData = array(), $logData2 = array())
+    {
+        self::_log(LOG_EMERG, func_get_args());
+    }
+
+    public static function CRIT($logData = array(), $logData2 = array())
+    {
+        self::_log(LOG_CRIT, func_get_args());
+    }
+
+    public static function ERR($logData = array(), $logData2 = array())
+    {
+        self::_log(LOG_ERR, func_get_args());
+    }
+
+    public static function WARNING($logData = array(), $logData2 = array())
+    {
+        self::_log(LOG_WARNING, func_get_args());
+    }
+
+    public static function NOTICE($logData = array(), $logData2 = array())
+    {
+        self::_log(LOG_NOTICE, func_get_args());
+    }
+
+    public static function INFO($logData = array(), $logData2 = array())
+    {
+        self::_log(LOG_INFO, func_get_args());
+    }
+
+    public static function DEBUG($logData = array(), $logData2 = array())
+    {
+        self::_log(LOG_DEBUG, func_get_args());
+    }
+
+    private static function _log($logType = LOG_INFO, $logArgs = array())
+    {
+        //未定义debug模式时，当log的级别大于信息6（LOG_DEBUG => 'DEBUG', 7）
+        if (!(defined('DEBUG') && DEBUG > 0) && $logType > LOG_INFO) {
+            return;
+        }
+
+        $logLevel = isset(self::$logLevel[$logType]) ? self::$logLevel[$logType] : '';
+        $logs = ['time' => date("Y-m-d H:i:s"), 'LEVEL' => $logLevel];
+
+        foreach ($logArgs as $logData) {
+            foreach ((array)$logData as $key => $value) {
+                if ($key === 'DEBUG') {
+                    $value = '<<<<' . join("\t", (array)$value) . '>>>>';
+                } else {
+                    if (is_array($value) || is_object($value)) {
+                        $value = json_encode($value, JSON_UNESCAPED_UNICODE);
+                    }
+                }
+                $value = str_replace("/\b+/", ' ', $value);
+                if (is_int($key)) {
+                    $logs[] = $value;
+                } else {
+                    $logs[] = $key . ':' . $value;
+                }
+            }
+        }
+        $logStr = join("]\t[", $logs) . "\n";
+        self::_cacheLog($logLevel, $logArgs);
+        self::_writeLog($logLevel, $logStr);
+        if ($logType <= LOG_ERR) {
+            error_log($logStr);
+        }
+    }
+
+    private static function _getFilePath()
+    {
+        if (!empty(self::$_LOGGER_DIR)) {
+            $dir = self::$_LOGGER_DIR;
+            if ($dir[strlen($dir) - 1] != "/") {
+                $dir .= "/";
+            }
+            return $dir . "wechaty_log-" . date("Ymd");
+        } else {
+            return self::LOGGER_PREFIX . "wechaty_log-" . date("Ymd");
+        }
+    }
+
+    private static function _writeLog($logLevel, $logStr)
+    {
+        $logFile = self::_getFilePath();
+        if (!file_exists($logFile)) {
+            self::_mkfile($logFile);
+        }
+        if (PHP_SAPI == 'cli') {
+            file_put_contents($logFile, $logStr, FILE_APPEND);
+        } else {
+            file_put_contents($logFile, $logStr, FILE_APPEND);
+        }
+    }
+
+    private static function _cacheLog($logLevel, $log)
+    {
+        if (!defined('DEBUG') || DEBUG <= 0) {
+            return;
+        }
+        if ('cli' == PHP_SAPI) {
+            print_r($log);
+            return;
+        }
+        if (count(self::$_logs) < 40) {
+            self::$_logs[] = [$logLevel, $log];
+        }
+    }
+
+    private static function _mkfile($logFile)
+    {
+        if (!file_exists($logFile)) {
+            $logFolder = dirname($logFile);
+            if (!is_dir($logFolder)) {
+                if (!self::Mkdirs($logFolder)) {
+                    return false;
+                }
+            }
+            touch($logFile);
+            chmod($logFile, 0777);
+        }
+        return true;
+    }
+
+    private static function Mkdirs($dir)
+    {
+        if (!is_dir($dir)) {
+            if (!self::Mkdirs(dirname($dir))) {
+                return false;
+            }
+            if (!mkdir($dir, 0777)) {
+                return false;
+            } else {
+                chmod($dir, 0777);
+            }
+        }
+        return true;
+    }
+}
